e5f534ec1085b9174589fd58a1b434d417bdf94a
SixFourDev <sb_94@yahoo.com>
2023-09-12 21:03:45 -0700

    added mutations.js and fixed App.jsx

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,28 @@
+import { gql } from '@apollo/client';
+
+// Define your LOGIN_USER mutation
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
