99e05a9da011207e004ae208b86973fb66d56df4
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-09-14 15:32:06 -0700

    routes and styling development begun

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,78 +1,126 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-
-// If we use bootstrap for our signup form, uncomment next line. bootstrap classNames are already applied below.
-import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
-import SignUpForm from './SignupForm';
-import LoginForm from './LoginForm';
-
-import Auth from '../utils/auth';
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Auth from "../utils/auth";
+import "../App.css";
 
 const AppNavbar = () => {
+  const [currentPage, handlePageChange] = useState("Home");
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [isNavOpen, setIsNavOpen] = useState(false); // Add state for the mobile menu
 
-  const [showModal, setShowModal] = useState(false);
+  // Update the screen width state when the window is resized
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
+  const userLoggedIn = Auth.loggedIn();
 
   return (
-    <>
-      <Navbar bg='dark' variant='dark' expand='lg'>
-        <Container fluid>
-          <Navbar.Brand as={Link} to='/'>
-            NPM Run Start
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls='navbar' />
-          <Navbar.Collapse id='navbar' className='d-flex flex-row-reverse'>
-            <Nav className='ml-auto d-flex'>
-              <Nav.Link as={Link} to='/'>
-                Search Shoes
-              </Nav.Link>
-              
-              {Auth.loggedIn() ? (
-                <>
-                  <Nav.Link as={Link} to='/saved'>
-                    See Your Shoes
-                  </Nav.Link>
-                  <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
-                </>
-              ) : (
-                <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      
-      <Modal
-        size='lg'
-        show={showModal}
-        onHide={() => setShowModal(false)}
-        aria-labelledby='signup-modal'>
-        
-        <Tab.Container defaultActiveKey='login'>
-          <Modal.Header closeButton>
-            <Modal.Title id='signup-modal'>
-              <Nav variant='pills'>
-                <Nav.Item>
-                  <Nav.Link eventKey='login'>Login</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Tab.Content>
-              <Tab.Pane eventKey='login'>
-                <LoginForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-              <Tab.Pane eventKey='signup'>
-                <SignUpForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-            </Tab.Content>
-          </Modal.Body>
-        </Tab.Container>
-      </Modal>
-    </>
+    <nav>
+      {/* Render hamburger menu icon for screen width <= 600px */}
+      {screenWidth <= 600 ? (
+        <div
+          className={`navbar-toggler ${isNavOpen ? "active" : ""}`}
+          onClick={toggleNav}
+        >
+          <div className="icon-bar"></div>
+          <div className="icon-bar"></div>
+          <div className="icon-bar"></div>
+        </div>
+      ) : (
+        // Conditionally render navigation links based on screen width
+        <ul className={`nav-links ${isNavOpen ? "open" : ""}`}>
+          <li>
+            <Link
+              to="/"
+              onClick={() => {
+                handlePageChange("Home");
+                setIsNavOpen(false);
+              }}
+              className={currentPage === "Home" ? "active" : ""}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/login"
+              onClick={() => {
+                handlePageChange("Login");
+                setIsNavOpen(false);
+              }}
+              className={currentPage === "Login" ? "active" : ""}
+            >
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/signup"
+              onClick={() => {
+                handlePageChange("Signup");
+                setIsNavOpen(false);
+              }}
+              className={currentPage === "Signup" ? "active" : ""}
+            >
+              Signup
+            </Link>
+          </li>
+        </ul>
+      )}
+
+      <ul>
+        {userLoggedIn ? (
+          <>
+            <li>
+              <Link
+                to="/"
+                onClick={() => {
+                  Auth.logout();
+                  setIsNavOpen(false); // Close the mobile menu when a link is clicked
+                }}
+                className="logout"
+              >
+                Logout
+              </Link>
+            </li>
+            <li>
+              <Link to="/signup" className="signup">
+                Signup
+              </Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/" className="home">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/login" className="login">
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link to="/signup" className="signup">
+                Signup
+              </Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
   );
 };
 
