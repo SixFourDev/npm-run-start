d7d50998d26f5e0c711a9a9cf499ad7eaaa6b69d
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-09-17 16:02:03 -0700

    Header, Nav and Footer styled

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,33 +1,21 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import AppNavbar from './NavBar';
-
-//Do we need to import useState from 'react'?
-// import SignupForm from './SignupForm';
-// import LoginForm from './LoginForm';
+import logo from '../../../assets/logo.svg';
 
 import Auth from '../utils/auth';
 
 import "../App.css";
 
-// import NPMRS "logo"
-
 export default function Header() {
     return (
         <header>
             <div id="header-logo">
-                <h1>NPM</h1>
-                <div id="slogan">
-                <h2>Your Best <span>Run Start</span>s Here!</h2>
+                <img src={ logo } alt="NPM Shoes Logo"/>
             </div>
-            </div>
-
-
             <div id="nav">
                 <AppNavbar />
             </div>
-
-            
         </header>
     );
 }
